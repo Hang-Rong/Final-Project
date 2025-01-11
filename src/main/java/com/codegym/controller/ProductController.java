1a2ff7396914bc5e2d4481ca9dbf2ff4debf651a
truongthuan <truongthuan.r@gmail.com>
2025-01-11 17:56:10 +0700

    solve conflict + init OrderController

--- src/main/java/com/codegym/controller/ProductController.java
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -5,14 +5,10 @@ import com.codegym.repository.IProductRepository;
 import com.codegym.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 import java.util.List;
 
@@ -30,7 +26,6 @@ public class ProductController {
         return modelAndView;
     }
 //Lưu sp moi
-//Lưu sp moi
     @PostMapping("/create")
     public ModelAndView saveProduct(@ModelAttribute("product") Product product) {
         productService.save(product);
@@ -41,43 +36,7 @@ public class ProductController {
     // Hiển thị ds sp
     @GetMapping
     public ModelAndView listProducts(@RequestParam(value = "page", defaultValue = "0") int page,
-                                     @RequestParam(value = "size", defaultValue = "5") int size) {
-        ModelAndView modelAndView = new ModelAndView("/product/list");
-        modelAndView.addObject("products", productService.findAll(PageRequest.of(page, size)));
-        return modelAndView;
-    }
-
-    // Hiển thị form update
-    @GetMapping("/edit/{id}")
-    public ModelAndView showEditForm(@PathVariable("id") Long id) {
-        Product product = productService.findById(id).orElse(null);
-        ModelAndView modelAndView = new ModelAndView("/product/edit");
-        modelAndView.addObject("product", product);
-        return modelAndView;
-    }
-
-    // Lưu chỉnh sửa sp (Update)
-    @PostMapping("/edit/{id}")
-    public ModelAndView updateProduct(@PathVariable("id") Long id, @ModelAttribute("product") Product product) {
-        product.setId(id);  // Đảm bảo ID không bị thay đổi khi chỉnh sửa
-        productService.save(product);
-        return new ModelAndView("redirect:/products");
-    }
-
-    // Xóa sp (Delete)
-    @GetMapping("/delete/{id}")
-    public ModelAndView deleteProduct(@PathVariable("id") Long id) {
-        productService.remove(id);
-        return new ModelAndView("redirect:/products");
-    }
-    // Hiển thị ds sp
-    @GetMapping
-    public ModelAndView listProducts(@RequestParam(value = "page", defaultValue = "0") int page,
-<<<<<<< HEAD
                                      @RequestParam(value = "size", defaultValue = "3") int size) {
-=======
-                                     @RequestParam(value = "size", defaultValue = "5") int size) {
->>>>>>> 57f7fda445cb61cd7042ca40aa230d1f4cab130f
         ModelAndView modelAndView = new ModelAndView("/product/list");
         modelAndView.addObject("products", productService.findAll(PageRequest.of(page, size)));
         return modelAndView;
@@ -95,8 +54,7 @@ public class ProductController {
     // Lưu chỉnh sửa sp (Update)
     @PostMapping("/edit/{id}")
     public ModelAndView updateProduct(@PathVariable("id") Long id, @ModelAttribute("product") Product product) {
-        product.setId(id);  // Đảm bảo ID không bị thay đổi khi chỉnh sửa
-
+        product.setId(id);
         productService.save(product);
         return new ModelAndView("redirect:/products");
     }
@@ -117,5 +75,4 @@ public class ProductController {
         modelAndView.addObject("searchName", name);
         return modelAndView;
     }
-
 }
--- src/main/java/com/codegym/model/Order.java
+++ src/main/java/com/codegym/model/Order.java
@@ -3,28 +3,6 @@ package com.codegym.model;
 import lombok.Data;
 
 import jakarta.persistence.*;
-
-import java.util.Date;
-
-@Entity
-@Table(name = "order")
-@Data
-
-public class Order {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private Date date;
-    private Double total;
-    private String paymentMethod;
-    private String status;
-}
-
-package com.codegym.model;
-
-import lombok.Data;
-
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
