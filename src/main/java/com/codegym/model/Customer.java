6d6802de3b68d4acbc670a53c1829d1656e8611f
truongthuan <truongthuan.r@gmail.com>
2025-02-20 16:57:47 +0700

    temp

--- src/main/java/com/codegym/model/Customer.java
+++ src/main/java/com/codegym/model/Customer.java
@@ -1,30 +1,28 @@
 package com.codegym.model;
 
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.*;
 
 import java.util.Date;
 
 @Entity
-@Table(name = "user")
+@Table(name = "customer")
 @Data
-public class User {
+public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String phone;
-
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date birthday;
     private String email;
     private String address;
-    private String avatarImage;
+//    private String avatarImage;
 
 
-//    @OneToOne(mappedBy = "customer")
-//    private AppUser appUser;
+    @OneToOne(mappedBy = "customer")
+    private AppUser appUser;
 
     private boolean isDeleted = false;
 }
